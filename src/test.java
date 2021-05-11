297a605e0db73349e8214a7ddecfe9795d59cb54
aidthebest <82460114+aidthebest@users.noreply.github.com>
2021-05-11 17:35:50 +0300

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
     int InitialClientBalance = 2_000_000_000;
-    int BalanceTopUp = 500_000_000;
+    int BalanceTopUp = 50_000_000;
     int CurrentBalance = InitialClientBalance + BalanceTopUp;
     System.out.println(CurrentBalance);
 }
