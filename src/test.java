25df368db8f6be3c17966c424fd460881c3f1f91
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-05-11 17:32:28 +0300

    aa

---
+++ src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+    int InitialClientBalance = 2_000_000_000;
+    int BalanceTopUp = 500_000_000;
+    int CurrentBalance = InitialClientBalance + BalanceTopUp;
+    System.out.println(CurrentBalance);
+}
+}
